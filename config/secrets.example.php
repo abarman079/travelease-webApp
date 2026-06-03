71c0736e1ee75f28d163fb5ac2308924528c5df2
Md. Akibul Hasan Arman <138577105+abarman079@users.noreply.github.com>
2026-06-04 02:28:32 +0600

    Add example secrets configuration file

---
+++ config/secrets.example.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'DB_HOST' => '',
+    'DB_PORT' => '3306',
+    'DB_NAME' => '',
+    'DB_USER' => '',
+    'DB_PASS' => '',
+
+    'MAIL_FROM_ADDRESS' => '',
+    'MAIL_FROM_NAME' => 'TravelEase',
+    'MAIL_SMTP_HOST' => 'smtp.gmail.com',
+    'MAIL_SMTP_PORT' => 587,
+    'MAIL_SMTP_USERNAME' => '',
+    'MAIL_SMTP_PASSWORD' => '',
+];
